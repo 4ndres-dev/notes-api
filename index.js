5789b61c2f21670ea951f3649b213431a99b6911
Andrés <4ndrei.93@gmail.com>
2021-05-10 00:17:12 +0200

    Keroku init

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/api/notes', (request, response) => {
 	response.status(201).json(newNote);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Iniciando el servidor en el puerto ${PORT}`);
 });
